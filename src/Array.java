80a8f12478c3e8d0cf3b880716638c939e2c3396
ruoxigeng <gengruoxi2013@gmail.com>
2019-11-14 22:07:37 +0800

    Array

--- src/Array.java
+++ src/Array.java
@@ -1,11 +1,11 @@
-public class Array {
+public class Array<E> {
 
-    private int[] data;
+    private E[] data;
     private int size;
 
     //构造函数
     public Array(int capacity) {
-        data = new int[capacity];
+        data = (E[]) new Object[capacity];
         size = 0;
     }
 
@@ -27,23 +27,104 @@ public class Array {
     }
 
     //向所有元素最后加一个新元素
-    public void addLast(int e) {
+    public void addLast(E e) {
         add(size, e);
     }
 
-    public void addFirst(int e) {
+    public void addFirst(E e) {
         add(0, e);
     }
 
     //在第index位置插入元素e
-    public void add(int index, int e) {
-        if(size == data.length)
-            throw new IllegalArgumentException("Add failed. Array is full");
+    public void add(int index, E e) {
         if(index < 0 || index >size)
             throw new IllegalArgumentException("Add failed.");
+
+        if(size == data.length)
+            resize(2 * data.length);
+
         for(int i = size - 1; i >= index; i--)
             data[i + 1] = data[i];
+
         data[index] = e;
         size++;
+    }
+
+    public E get(int index) {
+        if(index < 0 || index >size)
+            throw new IllegalArgumentException("Get failed.");
+        return data[index];
+    }
+
+    public void set(int index, E e) {
+        if(index < 0 || index >size)
+            throw new IllegalArgumentException("Set failed.");
+        data[index] = e;
+    }
+
+    public boolean contains(E e) {
+        for(int i = 0; i < size; i++) {
+            if(data[i].equals(e))
+                return true;
+        }
+        return false;
+    }
+
+    public int find(E e) {
+        for(int i = 0; i < size; i++) {
+            if(data[i].equals(e))
+                return i;
+        }
+        return -1;
+    }
+
+    public E remove(int index) {
+        if(index < 0 || index >size)
+            throw new IllegalArgumentException("Remove failed.");
+
+        E ret = data[index];
+        for(int i = index + 1; i < size; i++)
+            data[i-1] = data[i];
+        size--;
+        data[size] = null;
+
+        if(size == data.length / 4 && data.length / 2 != 0)
+            resize(data.length / 2);
+        return ret;
+    }
+
+    public E removeFirst() {
+        return remove(0);
+    }
+
+    public E removeLast() {
+        return remove(size - 1);
+    }
+
+    public void removeElement(E e) {
+        int index = find(e);
+        if(index != -1)
+            remove(index);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append(String.format("Array: size = %d , capacity = %d\n", size, data.length));
+        res.append('[');
+        for(int i = 0; i < size; i++) {
+            res.append(data[i]);
+            if(i != size - 1)
+                res.append(" , ");
+        }
+        res.append(']');
+        return res.toString();
+    }
+
+    private void resize(int newCapacity) {
+        E[] newData = (E[]) new Object[newCapacity];
+        for(int i = 0; i < size; i++)
+            newData[i] = data[i];
+        data = newData;
     }
 }
